14a1e39fc81a9df0c19410103d3e781476840069
MickeyKunakorn <41740160+MickeyKunakorn@users.noreply.github.com>
2025-11-04 16:02:36 +0700

    Update build.sh

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,1 +1,13 @@
-
+args=("$@")
+path=$(pwd)
+if (($# == 0))
+then
+    echo "No argument supplied"
+else
+    for i; do
+        DIR_NAME="ex$i"
+        if [ ! -d "DIR_NAME" ]; then
+            mkdir "$DIR_NAME"
+        fi
+    done
+fi
